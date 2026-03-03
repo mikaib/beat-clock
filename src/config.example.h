1bf8503a998d0386893ea52e7f9b787e89c948b2
mikaib <mikaib@duck.com>
2026-03-03 17:09:41 +0100

    [driver/wifi] config + outline

---
+++ src/config.example.h
@@ -0,0 +1,20 @@
+//
+// Created by mikaib on 03-03-2026.
+//
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// oled settings
+#define OLED_ADDR 0x3C
+#define OLED_SPEED 400000
+#define OLED_SDA GPIO_NUM_4
+#define OLED_SCL GPIO_NUM_5
+#define OLED_WIDTH 128
+#define OLED_HEIGHT 64
+
+// wifi settings
+#define WIFI_SSID "haha"         //! REQUIRED
+#define WIFI_PASSWORD "you_wish" //! REQUIRED
+
+#endif // CONFIG_H
