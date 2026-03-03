463dc192a9dd51571d18c5c40fbdc7b9404f0254
mikaib <mikaib@duck.com>
2026-03-03 17:15:02 +0100

    [driver/wifi] adapt implementation from weather station

--- src/main.c
+++ src/main.c
@@ -1,9 +1,10 @@
-#include <stdio.h>
-
-#include "driver/oled.h"
+#include <driver/oled.h>
+#include <driver/wifi.h>
 
 void app_main() {
     driver_oled_init();
+    driver_wifi_init();
+
     driver_oled_clear();
     driver_oled_draw_text(10, 10, false, "Hello, World!");
     driver_oled_submit();
