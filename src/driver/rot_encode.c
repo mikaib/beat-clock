6e69b04693ae407430d9e5ca807f88771a87aa8a
mikaib <mikaib@duck.com>
2026-03-03 17:45:01 +0100

    [driver/encoder] make polling use struct

--- src/driver/rot_encode.c
+++ src/driver/rot_encode.c
@@ -5,6 +5,7 @@
 #include <driver/rot_encode.h>
 
 static int32_t counter_acc = 0; // acc(umulated since last poll)
+static bool button_pressed = false;
 
 void driver_rot_encoder_init() {
 
@@ -12,11 +13,14 @@ void driver_rot_encoder_init() {
 
 void driver_rot_encoder_flush() {
     counter_acc = 0;
+    button_pressed = false;
 }
 
-int32_t driver_rot_encoder_poll() {
+encoder_state_t driver_rot_encoder_poll() {
     int32_t acc = counter_acc;
+    bool pressed = button_pressed;
+
     driver_rot_encoder_flush();
 
-    return acc;
+    return { acc, pressed };
 }
--- src/driver/rot_encode.h
+++ src/driver/rot_encode.h
@@ -6,9 +6,15 @@
 #define ROT_ENCODE_H
 
 #include <stdint.h>
+#include <stdbool.h>
+
+typedef struct encoder_state {
+    int32_t steps;
+    bool button_pressed;
+} encoder_state_t;
 
 void driver_rot_encoder_init();
 void driver_rot_encoder_flush(); // flushes internal counter, so you don't get huge numbers after not polling for a while
-int32_t driver_rot_encoder_poll(); // returns amounts of steps taken since last poll
+encoder_state_t driver_rot_encoder_poll(); // returns amounts of steps taken since last poll
 
 #endif // ROT_ENCODE_H
